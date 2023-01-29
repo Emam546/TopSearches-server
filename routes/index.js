9041beb5e2c145823cd2879fdfaa564698c012dc
Emam546 <emam54637@gmail.com>
2023-01-29 20:15:30 +0200

    update trends

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,38 @@ var router = express.Router();
 const googleTrends = require('google-trends-api');
 const cache=require("../middleWar/cache")
 /* GET home page. */
-router.get('/trends',cache(), async function(req, res, next) {
+router.use(cache())
+router.get('/trends/:id', async function(req, res, next) {
+  const id=req.params.id || "US"
+  googleTrends.realTimeTrends({geo:id.toUpperCase()})
+  .then(function(results){
+    try{
+      console.log(results);
+      res.status(200).json({status:true,msg:"success",results:JSON.parse(results)})
+    }catch(err){
+      res.status(404).sendFile("public/404/index.html")
+    }
+  })
+  .catch(function(err){
+    next()
+  });
+});
+router.get('/trends', async function(req, res, next) {
   googleTrends.realTimeTrends({geo:"US"})
   .then(function(results){
     try{
       console.log(results);
       res.status(200).json({status:true,msg:"success",results:JSON.parse(results)})
     }catch(err){
-      res.status(400).json({status:false,msg:`Error Happened in parsing the data`})
-      console.error(err);
+      res.status(404).sendFile("public/404/index.html")
     }
   })
   .catch(function(err){
-    res.status(400).json({status:false,msg:`Error Happened in getting data`})
-
+    next()
   });
 });
+router.use((req,res)=>{
+  res.status(404).json({status:false,msg:`the data not found`})
+})
 
 module.exports = router;
